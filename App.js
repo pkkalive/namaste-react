dc4e7004023229877cef3af5f417581d4e4116b0
pkkalive <kalivepurushothamkumar@gmail.com>
2024-11-08 00:06:14 -0500

    added nested react script

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Child 1 H1"),
-    React.createElement("h2", {}, "Child 1 H2"),
+    React.createElement("h1", {}, "I'm child 1 H1"),
+    React.createElement("h2", {}, "I'm child 1 H2"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Child 2 H1"),
-    React.createElement("h2", {}, "Child 2 H2"),
+    React.createElement("h1", {}, "I'm child 2 H1"),
+    React.createElement("h2", {}, "I'm child 2 H2"),
   ]),
 ]);
 
