6b4e627481b9e798bccae15eff27fbddc281cdf9
pkkalive <kalivepurushothamkumar@gmail.com>
2024-11-09 19:39:16 -0500

    building react foundation

--- App.js
+++ App.js
@@ -1,16 +1,2 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm child 1 H1"),
-    React.createElement("h2", {}, "I'm child 1 H2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm child 2 H1"),
-    React.createElement("h2", {}, "I'm child 2 H2"),
-  ]),
-]);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
