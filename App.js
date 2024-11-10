89101a9de686b9a7596af7efd66518383bf20e69
pkkalive <kalivepurushothamkumar@gmail.com>
2024-11-10 01:05:57 -0500

    Basic React using JSX

--- App.js
+++ App.js
@@ -1,2 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+const Title = () => {
+  return (
+    <h1 className="title" tabIndex="5">
+      Namaste React using JSX 🚀
+    </h1>
+  );
+};
+
+// Component Composition: a component inside a component like here title is inside heading
+const Heading = () => {
+  return (
+    <header className="header">
+      <Title />
+      <h1 className="heading">Namaste React Functional Component</h1>
+    </header>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Heading />);
